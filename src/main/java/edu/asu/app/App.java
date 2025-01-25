f7f367c57384a5aa4afb5f2556ecb067bf53d704
Shane McGuirt <10144278+srmcguirt@users.noreply.github.com>
2025-01-25 17:22:30 +0000

    feat: update App to prompt for user input and display proper name

--- src/main/java/edu/asu/app/App.java
+++ src/main/java/edu/asu/app/App.java
@@ -1,12 +1,12 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
-
 package edu.asu.app;
+import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+        Scanner scnr = new Scanner(System.in);
+        String properName = "";
+        System.out.print("Enter a proper name : ");
+        properName = scnr.nextLine();
+        System.out.println(properName);
     }
 }
